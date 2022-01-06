5b13f75d9e1d3f493a9b64de0367ca73fec85804
Samer Balee <samerbalee@gmail.com>
2022-01-06 13:55:23 +1100

    saving in local storage and get items

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,27 @@
-var currentDayEl = $("#currentDay");
+$(setDayPlanner);
 
-function displayCurentDay() {
+function setDayPlanner() {
+    var currentDayEl = $("#currentDay");
     var today = moment().format("dddd, MMMM Do");
     currentDayEl.text(today);
+
+    var eachTimeBlock = $(".time-block");
+    eachTimeBlock.each(function(){
+    var timeBlockId = $(this).attr("id");
+    // console.log(this);
+    var savedBlockId = $("#" + timeBlockId + " textarea");
+    savedBlockId.text(localStorage.getItem(moment().format("dddd, MMMM Do, ") + "hour-" + timeBlockId));
+    });
+
+    $(".saveBtn").on("click" , descritionLocalSave);
 }
-displayCurentDay();
+
+function descritionLocalSave(event){
+    var currentHourId = $(this).parent().attr("id");
+    // console.log(this);
+    localStorage.setItem(moment().format("dddd, MMMM Do, ") + "hour-" + currentHourId , $("#" + currentHourId + " textarea").val());
+    console.log($("#" + currentHourId + " textarea").val());
+    // console.log(moment().format("dddd, MMMM Do, ") + "hour-" + currentHourId + moment().format("a") , $("#" + currentHourId + " textarea").val());
+    // console.log(currentHourId);
+}
+
